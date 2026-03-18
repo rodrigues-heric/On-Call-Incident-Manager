c91713ecec1859496d26d2ff9cd10d0339368334
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-18 18:38:48 -0300

    Feat: adding project structure; PostgreSQL and REDIS from Compose (#8)
    * Feat: adding project structure folders
    
    * Update: using postgreSQL from docker compose
    
    * Feat: reading from .env

--- incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/IncidentmanagerApplication.java
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/IncidentmanagerApplication.java
@@ -3,11 +3,22 @@ package com.rodrigues.heric.incidentmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class IncidentmanagerApplication {
 
 	public static void main(String[] args) {
+		loadDotEnv();
+
 		SpringApplication.run(IncidentmanagerApplication.class, args);
+	}
+
+	private static void loadDotEnv() {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 	}
 
 }
