063a256c0d8d9f9425c60bb4734f7964ba1f987e
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-18 21:43:28 -0300

    Feat: adding audit, core domain and users entity

---
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.rodrigues.heric.incidentmanager.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = true)
+    private LocalDateTime deletedAt;
+}
---
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/repository/UsersRepository.java
@@ -0,0 +1,13 @@
+package com.rodrigues.heric.incidentmanager.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigues.heric.incidentmanager.domain.UsersEntity;
+
+@Repository
+public interface UsersRepository extends JpaRepository<UsersEntity, UUID> {
+
+}
