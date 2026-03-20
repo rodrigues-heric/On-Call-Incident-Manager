c5d806db8f40978e5ba8598f83ff33add1c8513c
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-20 14:07:48 -0300

    Test: user service; successfully create user

--- incidentmanager/src/test/java/service/UsersServiceTests.java
+++ incidentmanager/src/test/java/service/UsersServiceTests.java
@@ -1,13 +1,16 @@
 package service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.rodrigues.heric.incidentmanager.domain.UsersEntity;
 import com.rodrigues.heric.incidentmanager.dto.CreateUsersRequest;
+import com.rodrigues.heric.incidentmanager.dto.UsersDTO;
 import com.rodrigues.heric.incidentmanager.exception.BusinessException;
 import com.rodrigues.heric.incidentmanager.mapper.UsersMapper;
 import com.rodrigues.heric.incidentmanager.repository.UsersRepository;
@@ -35,8 +39,8 @@ public class UsersServiceTests {
     private UsersService usersService;
 
     @Test
-    @DisplayName("When the email is already in use it should throw Business Logic Exception (HTTP CONFLICT)")
-    public void whenEmailAlreadyInUse_thenUsersServiceShouldReturnConflictStatus() {
+    @DisplayName("When the email is already in use it should throw Business Logic Exception")
+    public void whenEmailAlreadyInUse_thenUsersServiceShouldThrowBusinessLogicException() {
         String email = "foo.bar@example.com";
         String name = "Foo Bar";
         String phone = "1234567890";
@@ -46,14 +50,39 @@ public class UsersServiceTests {
         when(usersRepository.findByEmail(email)).thenReturn(Optional.of(new UsersEntity()));
 
         BusinessException exception = assertThrows(BusinessException.class, () -> {
-            usersService.createUser(request);
+            this.usersService.createUser(request);
         });
 
         assertEquals("Email: " + email + " already in use.", exception.getMessage());
 
-        verify(usersRepository).findByEmail(email);
-        verify(usersMapper, never()).toEntity(any());
-        verify(usersRepository, never()).save(any());
+        verify(this.usersRepository).findByEmail(email);
+        verify(this.usersMapper, never()).toEntity(any());
+        verify(this.usersRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("Should create a user successfully if the data are valid")
+    public void shouldCreateUserSuccessfully() {
+        String email = "foo.bar@example.com";
+        String name = "Foo Bar";
+        String phone = "1234567890";
+
+        CreateUsersRequest request = new CreateUsersRequest(name, email, phone);
+        UsersEntity userEntity = new UsersEntity();
+        UsersEntity savedUser = new UsersEntity();
+        UsersDTO expectedDTO = new UsersDTO(UUID.randomUUID(), name, email, phone);
+
+        when(this.usersRepository.findByEmail(request.email())).thenReturn(Optional.empty());
+        when(this.usersMapper.toEntity(request)).thenReturn(userEntity);
+        when(this.usersRepository.save(userEntity)).thenReturn(savedUser);
+        when(this.usersMapper.toDTO(savedUser)).thenReturn(expectedDTO);
+
+        UsersDTO result = this.usersService.createUser(request);
+
+        assertNotNull(result);
+        assertEquals(expectedDTO.email(), result.email());
+
+        verify(this.usersRepository, times(1)).save(userEntity);
     }
 
 }
