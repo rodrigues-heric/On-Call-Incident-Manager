d7ead0826ca1444b6e74c78b766b19b3762f9dc8
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-23 10:09:47 -0300

    Feat: adding services entity

---
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/domain/ServicesEntity.java
@@ -0,0 +1,47 @@
+package com.rodrigues.heric.incidentmanager.domain;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.rodrigues.heric.incidentmanager.domain.enums.CriticalityEnum;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_services")
+@SQLDelete(sql = "UPDATE tb_services SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServicesEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String team;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CriticalityEnum criticality;
+
+}
