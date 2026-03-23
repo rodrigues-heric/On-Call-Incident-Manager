82c84dad58bd3b049b1bcd79088ad0118d901058
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-23 15:54:25 -0300

    Feat: adding create service

--- incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/controller/ServicesController.java
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/controller/ServicesController.java
@@ -2,14 +2,19 @@ package com.rodrigues.heric.incidentmanager.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rodrigues.heric.incidentmanager.dto.CreateServicesRequest;
 import com.rodrigues.heric.incidentmanager.dto.ServicesDTO;
 import com.rodrigues.heric.incidentmanager.service.ServicesService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -23,6 +28,12 @@ public class ServicesController {
     public ResponseEntity<List<ServicesDTO>> getAllServices() {
         List<ServicesDTO> result = this.servicesService.listAllServices();
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ServicesDTO> createService(@RequestBody @Valid CreateServicesRequest request) {
+        ServicesDTO result = this.servicesService.createService(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
 }
--- incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/dto/CreateServicesRequest.java
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/dto/CreateServicesRequest.java
@@ -3,12 +3,13 @@ package com.rodrigues.heric.incidentmanager.dto;
 import com.rodrigues.heric.incidentmanager.domain.enums.CriticalityEnum;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateServicesRequest(
-        @NotBlank(message = "Field NAME is required") String name,
+                @NotBlank(message = "Field NAME is required") String name,
 
-        @NotBlank(message = "Field TEAM is required") String team,
+                @NotBlank(message = "Field TEAM is required") String team,
 
-        @NotBlank(message = "Field CRITICALITY is required") CriticalityEnum criticality) {
+                @NotNull(message = "Field CRITICALITY is required") CriticalityEnum criticality) {
 
 }
