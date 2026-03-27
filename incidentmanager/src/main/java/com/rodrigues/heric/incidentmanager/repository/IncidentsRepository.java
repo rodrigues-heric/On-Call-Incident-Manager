6336ae51038d4711ad71a32e7b0e2b163c00e779
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-27 15:25:16 -0300

    Feat: adding create service

--- incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/repository/IncidentsRepository.java
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/repository/IncidentsRepository.java
@@ -1,7 +1,9 @@
 package com.rodrigues.heric.incidentmanager.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ import com.rodrigues.heric.incidentmanager.domain.IncidentsEntity;
 
 @Repository
 public interface IncidentsRepository extends JpaRepository<IncidentsEntity, UUID> {
+
+    @EntityGraph(attributePaths = { "service", "assignee" })
+    Optional<IncidentsEntity> findById(UUID id);
 
 }
--- incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/service/IncidentsService.java
+++ incidentmanager/src/main/java/com/rodrigues/heric/incidentmanager/service/IncidentsService.java
@@ -1,8 +1,17 @@
 package com.rodrigues.heric.incidentmanager.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.rodrigues.heric.incidentmanager.domain.IncidentsEntity;
+import com.rodrigues.heric.incidentmanager.domain.ServicesEntity;
+import com.rodrigues.heric.incidentmanager.domain.UsersEntity;
+import com.rodrigues.heric.incidentmanager.dto.CreateIncidentsRequest;
+import com.rodrigues.heric.incidentmanager.exception.ResourceNotFoundException;
 import com.rodrigues.heric.incidentmanager.repository.IncidentsRepository;
+import com.rodrigues.heric.incidentmanager.repository.ServicesRepository;
+import com.rodrigues.heric.incidentmanager.repository.UsersRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +21,36 @@ import lombok.RequiredArgsConstructor;
 public class IncidentsService {
 
     private final IncidentsRepository incidentsRepository;
+    private final ServicesRepository servicesRepository;
+    private final UsersRepository usersRepository;
 
     @Transactional
-    public void createIncident() {
+    public IncidentsEntity createIncident(CreateIncidentsRequest request) {
+        ServicesEntity service = this.findServiceById(request.serviceId());
+        UsersEntity assignee = this.findAssigneeById(request.assigneeId());
 
+        IncidentsEntity incident = IncidentsEntity.builder()
+                .title(request.title())
+                .description(request.description())
+                .status(request.status())
+                .criticality(request.criticality())
+                .service(service)
+                .assignee(assignee)
+                .build();
+        return this.incidentsRepository.save(incident);
+    }
+
+    private ServicesEntity findServiceById(UUID id) {
+        return this.servicesRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Service with id " + id.toString() + " not found"));
+    }
+
+    private UsersEntity findAssigneeById(UUID id) {
+        if (id == null)
+            return null;
+
+        return this.usersRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User with id " + id.toString() + " not found"));
     }
 
 }
